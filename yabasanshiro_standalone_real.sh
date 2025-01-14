3c097c7d3162c14ef64aad94d583d0ca81cad222
elan <elanpang@gmail.com>
2025-01-14 12:38:52 +0800

    init commit

---
+++ yabasanshiro_standalone_real.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo $0 $*
+EMU_DIR=/mnt/SDCARD/Emus/SS
+YA_DIR=$EMU_DIR/.yabasanshiro
+
+
+$EMU_DIR/cpufreq.sh
+$EMU_DIR/cpuswitch.sh
+
+cd $EMU_DIR/.yabasanshiro
+export HOME="$EMU_DIR/.yabasanshiro"
+
+BIOS_FILE="/mnt/SDCARD/BIOS/saturn_bios.bin"
+$YA_DIR/yabasanshiro -r 3 -i "$@" -b "$BIOS_FILE"
